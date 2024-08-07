496601bbe36ef8692564824abc0e68f5c5ff544d
Brian Franco <mastastealth+git@gmail.com>
2024-08-06 20:26:33 -0400

    better pwaness

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import './app.css'
-import App from './App.svelte'
+import "./app.css";
+import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById('app'),
-})
+	target: document.getElementById("app"),
+});
 
-export default app
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker
+		.register("/service-worker.js")
+		.then((registration) => {
+			console.log("Service Worker registered with scope:", registration.scope);
+		})
+		.catch((error) => {
+			console.error("Service Worker registration failed:", error);
+		});
+}
+
+export default app;
