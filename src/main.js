ed558a02e3ce4a8e296cfa3de00a685d96d40f93
Brian Franco <mastastealth+git@gmail.com>
2024-08-06 20:53:37 -0400

    I dont know anymore

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const app = new App({
 
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker
-		.register("/service-worker.js")
+		.register("/oath-campaign-companion/service-worker.js")
 		.then((registration) => {
 			console.log("Service Worker registered with scope:", registration.scope);
 		})
